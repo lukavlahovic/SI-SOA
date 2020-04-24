15bda54794b3d02a44db693829229076af8996ff
Luka Jelic <ljelic9017rn@raf.rs>
2020-04-24 10:17:27 +0200

    Pocetak dinamicke orkestracije

---
+++ provajder2/src/main/java/com/provajder2/provajder2/Provajder2Application.java
@@ -0,0 +1,13 @@
+package com.provajder2.provajder2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Provajder2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Provajder2Application.class, args);
+    }
+
+}
---
+++ provajder2/src/test/java/com/provajder2/provajder2/Provajder2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provajder2.provajder2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provajder2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
